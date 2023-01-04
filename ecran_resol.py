ca165bc7e59e3180a38b8925ed7e9717034b06f2
anaisInformaticienne <120333963+anaisInformaticienne@users.noreply.github.com>
2023-01-04 23:21:26 +0100

    Update ecran_resol.py
    la version d'où j'importe la classe InputBox

--- ecran_resol.py
+++ ecran_resol.py
@@ -10,22 +10,24 @@ FONT = pg.font.Font(None, 32)
 
 class InputBox:
 
-    def __init__(self, x, y, w, h, exp = "(D|G|A|R){2,}", text=''):
+    def __init__(self, x, y, w, h, question, exp = "(D|G|A|R){2,}", text=''):
         self.rect = pg.Rect(x, y, w, h)
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.color = COLOR_INACTIVE
-        self.color_text = "White"
+        self.color_text = "Black"
         self.text = text
         self.txt_surface = FONT.render(text, True, self.color_text)
         self.active = False
         self.ERREUR = False
         self.exp = exp
+        self.aff_question = FONT.render(question, True, "Black")
+        self.reponse = None
 
     def handle_event(self, event):
-        
+
         if event.type == pg.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 self.active = not self.active
@@ -36,7 +38,7 @@ class InputBox:
             if self.active:
                 if self.ERREUR :
                     self.text = ''
-                    self.color_text = "White"
+                    self.color_text = "Black"
                     self.ERREUR = False
                 if event.key == pg.K_RETURN:
                     if saisie_motif(self.exp,self.text) == False:
@@ -46,21 +48,22 @@ class InputBox:
 
                     else:
                         print(self.text)
-                        self.ERREUR = False
-                        self.text = ''
+                        self.reponse = self.text
+
                 elif event.key == pg.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
                 self.txt_surface = FONT.render(self.text, True, self.color_text)
-        
+
 
     def update(self):
         width = max(200, self.txt_surface.get_width()+10)
         self.rect.w = width
 
     def draw(self, screen):
-        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        screen.blit(self.aff_question, (self.x, self.y - self.h))
+        screen.blit(self.txt_surface, (self.x+5, self.y+5))
         pg.draw.rect(screen, self.color, self.rect, 2)
 
 def saisie_motif(e, chaine):### expression rationnelle, vérifie zone de texte
@@ -69,15 +72,10 @@ def saisie_motif(e, chaine):### expression rationnelle, vérifie zone de texte
     if m == None:
         return False
     return m.end() == len(chaine)
-    
-        
 
-def main():
+
+def QuestionClavier(input_boxes):
     clock = pg.time.Clock()
-    input_box1 = InputBox(100, 100, 140, 32)
-    input_box2 = InputBox(100, 150, 140, 32)
-    input_box3 = InputBox(100, 200, 140, 32, exp = "[0-9]+")
-    input_boxes = [input_box1, input_box2, input_box3]
     done = False
 
     while not done:
@@ -85,21 +83,20 @@ def main():
             if event.type == pg.QUIT:
                 done = True
             for box in input_boxes:
-                box.handle_event(event)
-                
+                if box.reponse == None :
+                    box.handle_event(event)
+
 
         for box in input_boxes:
             box.update()
-            
+
 
         screen.fill((30, 30, 30))
-        
+
         for box in input_boxes:
             box.draw(screen)
 
         pg.display.flip()
         clock.tick(30)
 
-if __name__ == '__main__':
-    main()
-    pg.quit()
+
