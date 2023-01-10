780b600a6e4004d74a355508578c73d5604f8d07
anaisInformaticienne <120333963+anaisInformaticienne@users.noreply.github.com>
2023-01-10 18:48:23 +0100

    Update colonies_painting_ant.py

--- colonies_painting_ant.py
+++ colonies_painting_ant.py
@@ -1,6 +1,6 @@
-#Same program as before but using circle drawing method and placing the ants into tigh groups close to each other
-import pygame 
-from collections import deque 
+
+import pygame
+from collections import deque
 from random import choice, randrange
 
 class Ant():
@@ -8,12 +8,12 @@ class Ant():
         self.app = app
         self.color = color
         self.x, self.y = pos
-        self.increments = deque([(1,0),(0,1),(-1,0),(0,-1)]) #on utilise deque qui réunit les propriétés de la pile et de la file 
+        self.increments = deque([(1,0),(0,1),(-1,0),(0,-1)]) #on utilise deque qui réunit les propriétés de la pile et de la file
 #La fourmi peut se déplacer dans 4 directions; (1,0), (0,1), (-1,0), (0,-1)
 #deque provides an O(1) time complexity for append and pop operations as compared to a list that provides O(n) time complexity.
-    def run(self): #définition des règles que doit suivre la fourmi pour se déplacer 
+    def run(self): #définition des règles que doit suivre la fourmi pour se déplacer
         value = self.app.grid[self.y][self.x] #position de la fourmi sur l'écran défini par app
-        self.app.grid[self.y][self.x] = not value 
+        self.app.grid[self.y][self.x] = not value
 
         SIZE = self.app.CELL_SIZE #On dessine non plus des carrés mais des ronds
         center = self.x * SIZE, self.y * SIZE
@@ -23,15 +23,15 @@ class Ant():
 
         self.increments.rotate(1) if value else self.increments.rotate(-1) #rotate deque de droite à gauche si 1 et gauche à droite si -1, dépend de la case sur laquelle se trouve la fourmi à la position d'avant
 #On peut définir la position initiale de façon random
-        dx, dy = self.increments[0] #le premier élément de deque est donc l'incrément du prochain pas de la fourmi 
+        dx, dy = self.increments[0] #le premier élément de deque est donc l'incrément du prochain pas de la fourmi
         self.x = (self.x + dx) % self.app.COLS #pour que la fourmi ne quitte pas les frontières du tableau on prend le reste
         self.y = (self.y + dy) % self.app.ROWS
 
-#On supposera que la fourmi commence son déplacement sur une case blanche 
+#On supposera que la fourmi commence son déplacement sur une case blanche
 
 
 class App():
-    def __init__(self,WIDTH=1375,HEIGHT=715,CELL_SIZE=6,COLOR=(0,0,0),SPEED=120,NUMBER=400):
+    def __init__(self,WIDTH=1400,HEIGHT=800,CELL_SIZE=6,COLOR=(0,0,0),SPEED=120,NUMBER=400):
         pygame.init()
         self.screen = pygame.display.set_mode([WIDTH,HEIGHT])
         self.clock = pygame.time.Clock()
@@ -55,13 +55,20 @@ class App():
     @staticmethod
     def get_color():
         channel = lambda: randrange(30,220)
-        return channel(), channel(), channel() 
+        return channel(), channel(), channel()
 
     def run(self):
-        while True:
+        A = 1
+        while A == 1:
             [ant.run() for ant in self.ant]
 
-            [exit() for i in pygame.event.get() if i.type == pygame.QUIT]
+
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT :
+                    exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE :
+                    A = 0
+
             pygame.display.flip()
             self.clock.tick(self.speed)
 
